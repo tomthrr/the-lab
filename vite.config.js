632f32118507ffea9fdbe6e89bb56021eab432a4
Tom thierry <tomthierry73@gmail.com>
2024-03-12 21:42:09 +0100

    update: file structure

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,34 @@
+import { resolve } from 'path'
+
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
+  server:
     {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+      host: true, // Open to local network and display URL
+      open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
-    build:
+  build:
     {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+      outDir: '../dist', // Output in the dist/ folder
+      emptyOutDir: true, // Empty the folder first
+      sourcemap: true, // Add sourcemap
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+          simpleGeometries: resolve(__dirname, 'src/simple-geometries.html'),
+          loadTextures: resolve(__dirname, 'src/load-textures.html'),
+          portfolioText: resolve(__dirname, 'src/3d-text.html'),
+          addLights: resolve(__dirname, 'src/add-lights.html'),
+          addShadows: resolve(__dirname, 'src/add-shadows.html'),
+          hauntedHouse: resolve(__dirname, 'src/create-scene-haunted-house.html'),
+          particlesWave: resolve(__dirname, 'src/particles-wave.html'),
+          galaxyGenerator: resolve(__dirname, 'src/galaxy-generator.html'),
+          scrollBase: resolve(__dirname, 'src/scroll-base.html'),
+          addPhysics: resolve(__dirname, 'src/add-physics.html'),
+          importModels: resolve(__dirname, 'src/import-models.html'),
+        }
+      },
     },
 }
