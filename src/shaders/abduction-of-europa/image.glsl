c72015ac268ffb5f6fa50d414cc93540e5e037ce
Tom Thierry <73913043+tomthrr@users.noreply.github.com>
2025-04-23 16:11:07 +0200

    add: new 3dmodel

--- src/shaders/abduction-of-europa/image.glsl
+++ src/shaders/abduction-of-europa/image.glsl
@@ -8,7 +8,7 @@ void main() {
     float mask = texture2D(tReveal, vUv).r;
     vec4 sourceColor = texture2D(tColor, vUv);
     vec3 gray = vec3(dot(sourceColor.rgb, vec3(0.299, 0.587, 0.114)));
-    vec3 fadedColor = mix(gray, sourceColor.rgb, 0.2);
+    vec3 fadedColor = mix(gray, sourceColor.rgb, 0.3);
 
     vec3 finalColor = mix(min(sourceColor.rgb, 1.), fadedColor, mask);
     gl_FragColor = vec4(finalColor, sourceColor.a);
