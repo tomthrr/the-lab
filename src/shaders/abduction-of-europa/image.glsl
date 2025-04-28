25829a3b16b8342ee8a5288245ee2bfaffebdfe6
Tom Thierry <73913043+tomthrr@users.noreply.github.com>
2025-04-29 00:20:31 +0200

    add: luminescence & normal map works ?

--- src/shaders/abduction-of-europa/image.glsl
+++ src/shaders/abduction-of-europa/image.glsl
@@ -7,8 +7,10 @@ varying vec2 vUv;
 void main() {
     float mask = texture2D(tReveal, vUv).r;
     vec4 sourceColor = texture2D(tColor, vUv);
-    vec3 gray = vec3(dot(sourceColor.rgb, vec3(0.299, 0.587, 0.114)));
-    vec3 fadedColor = mix(gray, sourceColor.rgb, 0.1);
+    float luminance = dot(sourceColor.rgb, vec3(0.299, 0.587, 0.114));
+    vec3 watercolor = mix(sourceColor.rgb, vec3(1.0), 0.5 * (1.0 - luminance));
+
+    vec3 fadedColor = mix(watercolor, sourceColor.rgb, 0.4);
 
     vec3 finalColor = mix(min(sourceColor.rgb, 1.), fadedColor, mask);
     gl_FragColor = vec4(finalColor, sourceColor.a);
